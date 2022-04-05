9d6e8500200aab07b1d45583bfb5885bee353e00
Sayam Gautam <sayamgautam1@gmail.com>
2022-04-05 16:57:42 +0930

    add each text value saved inside local storafe at it respected time, time as key and toDo as its value

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -9,15 +9,22 @@ currentDateEl.text(today);
 // hours are kept in 24 hour format as moment().hour() return in 24hr format
 let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 let currentHour = moment().hour();
+let saveBtnEl = $(".saveBtn");
 for (i = 0; i < hours.length; i++) {
   if (currentHour === hours[i]) {
-    console.log(i);
-    $(`#${i}`).addClass("present");
+    $(`#description${i}`).addClass("present");
   }
   if (currentHour > hours[i]) {
-    $(`#${i}`).addClass("past");
+    $(`#description${i}`).addClass("past");
   }
   if (currentHour < hours[i]) {
-    $(`#${i}`).addClass("future");
+    $(`#description${i}`).addClass("future");
   }
 }
+// to save a value
+saveBtnEl.on("click", function () {
+  let time = $(this).siblings(".hour").text();
+  let toDo = $(this).siblings(".description").val();
+  // set the item into local storage
+  localStorage.setItem(time, toDo);
+});
