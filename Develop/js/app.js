51b6292025e3123a6a80235d38c8302702e5cd91
Sayam Gautam <sayamgautam1@gmail.com>
2022-04-05 14:53:35 +0930

    add condiional statement to get past, present and future color from provided classes in css

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -4,3 +4,20 @@ let currentDateEl = $("#currentDay");
 
 let today = moment().format("dddd, MMMM Do");
 currentDateEl.text(today);
+
+// changing the color using current, past nad present classes inside text area
+// hours are kept in 24 hour format as moment().hour() return in 24hr format
+let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let currentHour = moment().hour();
+for (i = 0; i < hours.length; i++) {
+  if (currentHour === hours[i]) {
+    console.log(i);
+    $(`#${i}`).addClass("present");
+  }
+  if (currentHour > hours[i]) {
+    $(`#${i}`).addClass("past");
+  }
+  if (currentHour < hours[i]) {
+    $(`#${i}`).addClass("future");
+  }
+}
